294476ac400067d5e1a8101dd5228081cc6f155e
Donald Wu <yeukfei02@gmail.com>
2020-01-27 22:39:18 +0800

    update-home-ui-#8: update home ui

--- app/src/main/java/com/donaldwu/lunchpickerandroid/navbar/home/HomeFragment.kt
+++ app/src/main/java/com/donaldwu/lunchpickerandroid/navbar/home/HomeFragment.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.donaldwu.lunchpickerandroid.R
 
 class HomeFragment : Fragment() {
-
-    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,12 +21,75 @@ class HomeFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
-        homeViewModel =  ViewModelProviders.of(this).get(HomeViewModel::class.java)
-//        val textView: TextView = root.findViewById(R.id.text_home)
-//        homeViewModel.text.observe(this, Observer {
-//            textView.text = it
-//        })
+        handleFoodCategoryDropdown(root)
+
+        getCurrentLocation(root)
+
+        handlePlaceRadioButton(root)
+
+        handleCurrentLocationRadioButton(root)
+
+        handleLocationEditText(root)
+
+        handleSubmitButton(root)
+
+        handleClearButton(root)
 
         return root
+    }
+
+    private fun handleFoodCategoryDropdown(root: View) {
+
+    }
+
+    private fun getCurrentLocation(root: View) {
+
+    }
+
+    private fun handlePlaceRadioButton(root: View) {
+        val placeRadioButton: RadioButton = root.findViewById(R.id.place_radio_button)
+        placeRadioButton.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                val placeLinearLayout: LinearLayout = root.findViewById(R.id.place_linear_layout)
+                placeLinearLayout.visibility = View.VISIBLE
+
+                val needToShowLinearLayout: LinearLayout = root.findViewById(R.id.need_to_show_linear_layout)
+                needToShowLinearLayout.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun handleCurrentLocationRadioButton(root: View) {
+        val currentLocationRadioButton: RadioButton = root.findViewById(R.id.current_location_radio_button)
+        currentLocationRadioButton.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                val placeLinearLayout: LinearLayout = root.findViewById(R.id.place_linear_layout)
+                placeLinearLayout.visibility = View.GONE
+
+                val needToShowLinearLayout: LinearLayout = root.findViewById(R.id.need_to_show_linear_layout)
+                needToShowLinearLayout.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun handleLocationEditText(root: View) {
+        val locationEditText: EditText = root.findViewById(R.id.location_edit_text)
+        locationEditText.addTextChangedListener {
+
+        }
+    }
+
+    private fun handleSubmitButton(root: View) {
+        val submitButton: Button = root.findViewById(R.id.submit_button)
+        submitButton.setOnClickListener {
+
+        }
+    }
+
+    private fun handleClearButton(root: View) {
+        val clearButton: Button = root.findViewById(R.id.clear_button)
+        clearButton.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/config/Config.kt
@@ -0,0 +1,7 @@
+package config
+
+class Config {
+    companion object {
+        val ROOT_URL = "https://lunch-picker-api.herokuapp.com/api"
+    }
+}
