8d1c4fb9dc420a8a9106dca477bdfc888e2c081a
Donald Wu <yeukfei02@gmail.com>
2020-01-28 00:09:53 +0800

    update-home-ui-#8: update home ui dropdown data

--- app/src/main/java/com/donaldwu/lunchpickerandroid/navbar/home/HomeFragment.kt
+++ app/src/main/java/com/donaldwu/lunchpickerandroid/navbar/home/HomeFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.RadioButton
+import android.widget.*
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.donaldwu.lunchpickerandroid.R
+import org.json.JSONObject
+import server.Server
 
 class HomeFragment : Fragment() {
+
+    private val foodCategoryList = arrayListOf<String>()
+    private var selectedTerm = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,8 +40,86 @@ class HomeFragment : Fragment() {
         return root
     }
 
-    private fun handleFoodCategoryDropdown(root: View) {
+    private fun getFoodCategories() {
+        val response = Server.getCategories()
+        if (response != null && response.isNotEmpty()) {
+            val jsonObject = JSONObject(response)
+            val categories = jsonObject.getJSONArray("categories")
 
+            val foodList = arrayListOf<String>()
+            val restaurantsList = arrayListOf<String>()
+            val barsList = arrayListOf<String>()
+            val breakfastBrunchList = arrayListOf<String>()
+            for (a in 0 until categories.length()) {
+                val item = categories.getJSONObject(a)
+
+                val parentAliases = item.getJSONArray("parent_aliases")
+                for (b in 0 until parentAliases.length()) {
+                    val parentAliasesFirstItem = parentAliases.getString(b)
+                    if (parentAliasesFirstItem == "food") {
+                        val title = item.getString("title")
+                        foodList.add(title)
+                    }
+                    if (parentAliasesFirstItem == "restaurants") {
+                        val title = item.getString("title")
+                        restaurantsList.add(title)
+                    }
+                    if (parentAliasesFirstItem == "bars") {
+                        val title = item.getString("title")
+                        barsList.add(title)
+                    }
+                    if (parentAliasesFirstItem == "breakfast_brunch") {
+                        val title = item.getString("title")
+                        breakfastBrunchList.add(title)
+                    }
+                }
+            }
+
+            if (foodList.isNotEmpty()) {
+                foodList.forEach {
+                    foodCategoryList.add(it)
+                }
+            }
+            if (restaurantsList.isNotEmpty()) {
+                restaurantsList.forEach {
+                    foodCategoryList.add(it)
+                }
+            }
+            if (barsList.isNotEmpty()) {
+                barsList.forEach {
+                    foodCategoryList.add(it)
+                }
+            }
+            if (breakfastBrunchList.isNotEmpty()) {
+                breakfastBrunchList.forEach {
+                    foodCategoryList.add(it)
+                }
+            }
+        }
+    }
+
+    private fun handleFoodCategoryDropdown(root: View) {
+        getFoodCategories()
+
+        val spinner: Spinner = root.findViewById(R.id.food_category_dropdown)
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            root.context,
+            android.R.layout.simple_spinner_dropdown_item,
+            foodCategoryList
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                selectedTerm = selectedItem
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+            }
+        }
     }
 
     private fun getCurrentLocation(root: View) {
--- app/src/main/java/config/Config.kt
+++
@@ -1,7 +0,0 @@
-package config
-
-class Config {
-    companion object {
-        val ROOT_URL = "https://lunch-picker-api.herokuapp.com/api"
-    }
-}
